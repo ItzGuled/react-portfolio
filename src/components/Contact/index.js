dcb3840d1389f0350513e52d40ccfe48e394791f
itzguled <itzguled55337@gmail.com>
2022-05-19 18:41:43 -0500

    contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -39,7 +39,7 @@ function Contact() {
 
   return (
     <section className="form-look">
-      <h1 data-testid="h1tag">Contact me</h1>
+      
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
